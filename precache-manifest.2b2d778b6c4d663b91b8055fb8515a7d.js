17dec932b9799aff9794f7985378b108969e8c3f
AlickShabanovich <alikshabanovich@gmail.com>
2020-01-17 19:18:44 +0300

    Updates

---
+++ precache-manifest.2b2d778b6c4d663b91b8055fb8515a7d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b6db02aaa116b978bc80d335f2d2e5f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "10470cb4b02e95cd6a95",
+    "url": "/portfolio/static/css/main.dd1c4fc2.chunk.css"
+  },
+  {
+    "revision": "8729a5d703b7e4f06868",
+    "url": "/portfolio/static/js/2.61446046.chunk.js"
+  },
+  {
+    "revision": "10470cb4b02e95cd6a95",
+    "url": "/portfolio/static/js/main.8a4571f1.chunk.js"
+  },
+  {
+    "revision": "4bc79a2d54f74fff90c2",
+    "url": "/portfolio/static/js/runtime~main.f7d4dbe8.js"
+  },
+  {
+    "revision": "a54dc02d1b0ceaea0c5c9effa4eb1a0e",
+    "url": "/portfolio/static/media/html.a54dc02d.svg"
+  },
+  {
+    "revision": "40f40c533176da5bc95d8254c8eb5513",
+    "url": "/portfolio/static/media/js.40f40c53.svg"
+  },
+  {
+    "revision": "6934d4c4f57d65b6ee3afbfa8d1cec63",
+    "url": "/portfolio/static/media/linkedin2.6934d4c4.svg"
+  },
+  {
+    "revision": "3d346b49bb8ec302ef11dca73484fe14",
+    "url": "/portfolio/static/media/react.3d346b49.svg"
+  },
+  {
+    "revision": "9ca51d2ac295d3538a2173763f5c7e70",
+    "url": "/portfolio/static/media/telegram2.9ca51d2a.svg"
+  },
+  {
+    "revision": "0570caf12588f65bf6e85780ea23a436",
+    "url": "/portfolio/static/media/todolist.0570caf1.jpg"
+  },
+  {
+    "revision": "043f35e642ff28b639b4942a714344bc",
+    "url": "/portfolio/static/media/vader.043f35e6.jpeg"
+  }
+]);
